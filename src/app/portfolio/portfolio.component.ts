34b92b0d9c88958a4626caf6eca9367ac37bfd8d
Mo0olTo <mostafa.allam.de@gmail.com>
2025-01-19 00:48:34 +0200

    FirstProjectAPp

---
+++ src/app/portfolio/portfolio.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-portfolio',
+  imports: [],
+  templateUrl: './portfolio.component.html',
+  styleUrl: './portfolio.component.css'
+})
+
+
+export class PortfolioComponent {
+
+
+
+
+
+
+}
+
+
+   
+    
+   
+
+  
+
+
+
+
